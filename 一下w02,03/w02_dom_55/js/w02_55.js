2e1e0703110d7679dd2606ea8c54a90b4e2f9f14
210410055 <taiwanhero230@gmail.com>
2025-03-09 20:48:41 +0800

    W02-P1: refine the code in week 1 using ony one compute function

---
+++ 一下w02,03/w02_dom_55/js/w02_55.js
@@ -0,0 +1,101 @@
+const userInput = document.querySelector('.input-number');
+const addBtn = document.querySelector('.btn-add');
+const subtractBtn = document.querySelector('.btn-subtract');
+const multiplyBtn = document.querySelector('.btn-multiply');
+const divideBtn = document.querySelector('.btn-divide');
+
+const currentResultOutput = document.querySelector('.current-result');
+const currentCalculationOutput = document.querySelector('.current-calculation');
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+// console.log('userInput:', userInput);
+// console.log('addBtn:', addBtn);
+// console.log('currentResultOutput:', currentResultOutput);
+
+function getUserInput() {
+  return parseFloat(userInput.value);
+}
+
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+  console.log(text);
+}
+
+function compute(type) {
+  let calText;
+  let result = 0;
+  const operand1 = parseFloat(currentResult);
+  const operand2 = getUserInput();
+  switch (type) {
+    case 'add':
+      result = operand1 + operand2;
+      calText = `${operand1} + ${operand2} = ${result}`;
+      break;
+    case 'subtract':
+      result = operand1 - operand2;
+      calText = `${operand1} - ${operand2} = ${result}`;
+      break;
+    case 'divide':
+      if (operand2 === 0) alert('Cannot divide by zero');
+      else {
+        result = (operand1 / operand2).toFixed(2);
+        calText = `${operand1} / ${operand2} = ${result}`;
+      }
+      break;
+    case 'multiply':
+      result = operand1 * operand2;
+      calText = `${operand1} * ${operand2} = ${result}`;
+      break;
+    // default:
+    //   break;
+  }
+  currentResult = result;
+  outputResult(currentResult, calText);
+}
+
+addBtn.addEventListener('click', () => compute('add'));
+subtractBtn.addEventListener('click', () => compute('subtract'));
+multiplyBtn.addEventListener('click', () => compute('multiply'));
+divideBtn.addEventListener('click', () => compute('divide'));
+
+//const是常數，不能再次賦值
+//let是變數，可以再次賦值
+//var是變數，可以再次賦值
+
+// function add() {
+//   const operand1 = parseFloat(currentResult);
+//   const operand2 = getUserInput();
+//   currentResult = operand1 + operand2;
+//   const calText = `${operand1} + ${operand2} = ${currentResult}`;
+//   outputResult(currentResult, calText);
+// }
+
+// function subtract() {
+//   const operand1 = parseFloat(currentResult);
+//   const operand2 = getUserInput();
+//   currentResult = operand1 - operand2;
+//   const calText = `${operand1} - ${operand2} = ${currentResult}`;
+//   outputResult(currentResult, calText);
+// }
+
+// function multiply() {
+//   const operand1 = parseFloat(currentResult);
+//   const operand2 = getUserInput();
+//   currentResult = operand1 * operand2;
+//   const calText = `${operand1} * ${operand2} = ${currentResult}`;
+//   outputResult(currentResult, calText);
+// }
+
+// function divide() {
+//   const operand1 = parseFloat(currentResult);
+//   const operand2 = getUserInput();
+//   if (operand2 === 0) alert('Cannot divide by zero');
+//   else {
+//     currentResult = (operand1 / operand2).toFixed(2);
+//     const calText = `${operand1} / ${operand2} = ${currentResult}`;
+//     outputResult(currentResult, calText);
+//   }
+// }
